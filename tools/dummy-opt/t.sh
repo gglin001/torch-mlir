21d86ab63174f1ccb9bc45f1a1512461de5b9d86
Allen Guo <guosonglin001@gmail.com>
2024-04-19 06:47:51 +0000

    wip

--- tools/dummy-opt/t.sh
+++ tools/dummy-opt/t.sh
@@ -1,5 +1,8 @@
-dummy-opt \
-    --dummy-pass \
-    --mlir-print-ir-after-all \
-    -o tools/dummy-opt/dummy.opt.mlir \
-    tools/dummy-opt/dummy.mlir
+args=(
+  # --debug
+  --dummy-pass
+  --mlir-print-ir-after-all
+  -o tools/dummy-opt/dummy.opt.mlir
+  tools/dummy-opt/dummy.mlir
+)
+dummy-opt "${args[@]}"
