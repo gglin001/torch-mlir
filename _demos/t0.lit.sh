ff7de52b3d91a6d618e93824e08a5ea9b6cf9437
Allen Guo <guosonglin001@gmail.com>
2024-04-26 14:25:29 +0800

    wip

---
+++ _demos/t0.lit.sh
@@ -0,0 +1,18 @@
+args=(
+  build/test
+  --show-tests
+  --debug
+)
+lit "${args[@]}"
+
+###############################################################################
+
+args=(
+  build/test
+  -v -a
+  ## disable `FileCheck` first
+  # --filter "Dialect/TMTensor/convert_to_loops.mlir"
+  # --filter "Dialect/TMTensor/canonicalize.mlir"
+  --filter "Dialect/TMTensor/bufferize.mlir"
+)
+lit "${args[@]}" | tee _demos/lit.run.dirty.mlir
